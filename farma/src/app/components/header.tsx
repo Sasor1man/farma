5ea59331e97f1c294f480490132ddd01d6f24d99
PavelBomberov <pavelbomberov@gmail.com>
2025-05-24 14:18:04 +0300

    todo change props

--- farma/src/app/components/header.tsx
+++ farma/src/app/components/header.tsx
@@ -1,21 +1,20 @@
 "use client";
 
 import * as React from "react";
-import { Component } from "react";
 import HeaderTop from "./headerComp/HeaderTop";
 import HeaderBottom from "./headerComp/HeaderBottom";
 
-interface HeaderProps {}
-
-interface HeaderState {}
-
-export class Header extends React.Component<HeaderProps, HeaderState> {
-  state: Readonly<HeaderState> = {};
-
+export class Header extends React.Component {
   render() {
     return (
       <header className="h-[167px]">
-        <HeaderTop />
+        <HeaderTop
+          links={[]}
+          contacts={{
+            phone: "",
+            mail: "",
+          }}
+        />
         <HeaderBottom />
       </header>
     );
