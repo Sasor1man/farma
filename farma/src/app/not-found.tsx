4faf5f04f23640130a54eae35c533adf3efd153c
PavelBomberov <pavelbomberov@gmail.com>
2025-06-03 17:01:05 +0300

    add not found page

---
+++ farma/src/app/not-found.tsx
@@ -0,0 +1,11 @@
+import { FunctionComponent } from "react";
+
+const NotFound: FunctionComponent = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center flex-col">
+      <h1 className="text-4xl font-bold mb-4">404 – Page not found</h1>
+    </div>
+  );
+};
+
+export default NotFound;
