5c134be03e19c0cca6d52b6d5b3ebad47b79c053
Sasoriman <pavelbomberov@gmail.com>
2025-08-17 13:20:15 +0300

    add nav link pages

---
+++ farma/src/app/contacts/page.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+import { FunctionComponent } from 'react';
+
+ 
+const Page: FunctionComponent = () => {
+    return ( <div>*Contacts page</div> );
+}
+ 
+export default Page;
---
+++ farma/src/app/news/page.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+
+import { FunctionComponent } from 'react';
+
+ 
+const Page: FunctionComponent = () => {
+    return ( <div>*News page</div> );
+}
+ 
+export default Page;
